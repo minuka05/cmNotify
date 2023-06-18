a89355064bce05385425c8366308d81c08a26157
Minuka x <124237692+minuka05@users.noreply.github.com>
2023-06-18 23:23:18 +0530

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,28 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+name 'cmNotify'
+author 'Minuka
+description 'cmNotify - Notification script made for QB Core'
+
+ui_page {
+    'html/ui.html',
+}
+
+files {
+	'html/ui.html',
+	'html/js/app.js',
+	'html/css/style.css',
+	'config.js',
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+exports {
+	'DoShortHudText',
+	'DoHudText',
+	'DoLongHudText',
+	'DoCustomHudText',
+	'PersistentHudText',
+}
