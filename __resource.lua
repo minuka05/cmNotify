6b2237c40aa0962018d77e468542794aa45ffa0f
Minuka x <124237692+minuka05@users.noreply.github.com>
2023-06-18 23:58:42 +0530

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 name 'cmNotify'
-author 'Minuka
+author 'Minuka'
 description 'cmNotify - Notification script made for QB Core'
 
 ui_page {
